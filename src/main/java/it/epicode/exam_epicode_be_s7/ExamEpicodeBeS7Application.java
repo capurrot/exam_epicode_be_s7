795a0f222931fe31501329b6bc9f2589b75b1f0d
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-05-03 14:21:29 +0200

    Initial commit

---
+++ src/main/java/it/epicode/exam_epicode_be_s7/ExamEpicodeBeS7Application.java
@@ -0,0 +1,13 @@
+package it.epicode.exam_epicode_be_s7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamEpicodeBeS7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamEpicodeBeS7Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/exam_epicode_be_s7/ExamEpicodeBeS7ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.exam_epicode_be_s7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamEpicodeBeS7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
