34f929289ab52439904904fa12fb87d6b539f9b2
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-05-03 15:21:44 +0200

    auth folder

---
+++ src/main/java/it/epicode/exam_epicode_be_s7/auth/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,20 @@
+package it.epicode.exam_epicode_be_s7.auth;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Accesso non autorizzato: " + authException.getMessage());
+    }
+}
